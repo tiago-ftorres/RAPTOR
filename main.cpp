ef69f697aba52429fb609abc8ee32ad108a92a06
Maria Rabelo <mariaesrabelo@gmail.com>
2024-11-11 18:33:45 +0000

    txt instead of csv (:

--- main.cpp
+++ main.cpp
@@ -13,19 +13,22 @@ int main(int argc, char *argv[]) {
     std::cout << "GTFS Input Directory: ";
     std::getline(std::cin, inputDirectory);
   }
-
-  std::unordered_map<int, Agency> agencies = readAgencies(inputDirectory + "/agency.csv");
-  std::unordered_map<int, Calendar> calendars = readCalendars(inputDirectory + "/calendar.csv");
-  std::unordered_map<int, Route> routes = readRoutes(inputDirectory + "/routes.csv");
-  std::unordered_map<int, Stop> stops = readStops(inputDirectory + "/stops.csv");
-  std::unordered_map<int, Trip> trips = readTrips(inputDirectory + "/trips.csv");
-  std::unordered_map<std::pair<int, int>, StopTime, pair_hash> stop_times = readStopTimes(inputDirectory + "/stop_times.csv");
-
+  std::unordered_map<int, Agency> agencies = readAgencies(inputDirectory + "/agency.txt");
   std::cout << "Agencies number: " << agencies.size() << std::endl;
+
+  std::unordered_map<int, Calendar> calendars = readCalendars(inputDirectory + "/calendar.txt");
   std::cout << "Calendars number: " << calendars.size() << std::endl;
-  std::cout << "Stops number: " << stops.size() << std::endl;
-  std::cout << "Trips number: " << trips.size() << std::endl;
+
+  std::unordered_map<int, Route> routes = readRoutes(inputDirectory + "/routes.txt");
   std::cout << "Routes number: " << routes.size() << std::endl;
+
+  std::unordered_map<int, Stop> stops = readStops(inputDirectory + "/stops.txt");
+  std::cout << "Stops number: " << stops.size() << std::endl;
+
+  std::unordered_map<int, Trip> trips = readTrips(inputDirectory + "/trips.txt");
+  std::cout << "Trips number: " << trips.size() << std::endl;
+
+  std::unordered_map<std::pair<int, int>, StopTime, pair_hash> stop_times = readStopTimes(inputDirectory + "/stop_times.txt");
   std::cout << "Stop Times number: " << stop_times.size() << std::endl;
 
   Raptor raptor(stops, routes, trips, stop_times);
